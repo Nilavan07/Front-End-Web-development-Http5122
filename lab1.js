acd6cc7e7a4c14977ae1c6fcad42aed80de7f3fb
nilavan <seranilavan0710@gmail.com>
2024-09-18 18:00:04 -0400

    changes

---
+++ lab1.js
@@ -0,0 +1,12 @@
+//alert();
+//alert("hello world");
+
+//var firstgreeting = "hello world";
+//alert(firstgreeting);
+
+var myname = "Nilavan";
+var myage =24;
+var year =365;
+var days =myage*year;
+var txt="days old… more or less";
+alert(myname+days+txt);
